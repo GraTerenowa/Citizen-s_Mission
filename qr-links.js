495e5ad1547eb87446e4c9563c2b5a00cc608ab2
GraTerenowa <kubamazurek@questworld.pl>
2026-08-05 12:02:20 +0200

    Update QR link for 'cards' task to 'puzzles'

--- qr-links.js
+++ qr-links.js
@@ -2,5 +2,5 @@ window.QR_LINKS = {
   "photography": "https://github.com/GraTerenowa/Citizen-s_Mission/clue.html?task=photography",
   "crossword": "https://github.com/GraTerenowa/Citizen-s_Mission/clue.html?task=crossword",
   "tracking": "https://github.com/GraTerenowa/Citizen-s_Mission/clue.html?task=tracking",
-  "cards": "https://github.com/GraTerenowa/Citizen-s_Mission/clue.html?task=cards"
+  "cards": "https://github.com/GraTerenowa/Citizen-s_Mission/clue.html?task=puzzles"
 };
